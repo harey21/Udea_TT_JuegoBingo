9a57740cce8a407dd23bc4a4c47d63dde9d83263
harey21 <hareyzmanjarresr1211@gmail.com>
2025-07-25 13:05:10 -0500

    Se icieron cambios en todas las carpetas y el html

--- codigo/app.js
+++ codigo/app.js
@@ -1,13 +1,28 @@
-angular.module("JuegoBingo",[])
-.controller("ControladorJuegoBingo",
-function ($scope){
-  
-    $scope.cantor = new Cantor ();
-    $scope.iniciarJuego = function(){
+angular.module("JuegoBingo", [])
+    .controller("ControladorJuegoBingo",
+        function ($scope) {
 
-    }
-    $scope.sacarBalota = function(){
-        $scope.cantor.sacarBalota();
-    }
-}
-);
+            $scope.cantor = new Cantor();
+            $scope.jugadores = [];
+
+            $scope.iniciarJuego = function () {
+                const totalJugadores = 7;
+                $scope.jugadores = [];
+                for (let i = 0; i < totalJugadores; i++) {
+                    $scope.jugadores.push({
+                        nombre: "Jugador " + (i + 1),
+                        tablaJugador: new Tabla(),
+                        ganador: false
+                    });
+                }
+            }
+
+            $scope.sacarBalota = function () {
+                $scope.cantor.sacarBalota();
+
+                for (i = 0; i < $scope.jugadores.length; i++) {
+                    $scope.jugadores[i].tablaJugador.taparNumero($scope.cantor.ultimaBalota);
+                }
+            }
+        }
+    );
--- codigo/cantor.js
+++ codigo/cantor.js
@@ -2,6 +2,7 @@ function Cantor() {
 
     this.balotasSacadas = new Array(75).fill(false);
     this.ultimaBalota = 0;
+    this.pendientes = 75;
 
     this.tabla = [];
     for (var f = 0; f < 15; f++) {
@@ -10,28 +11,27 @@ function Cantor() {
             this.tabla[f][c] = c * 15 + f + 1;
         }
     }
+
     //metodo para iniciar
     this.iniciar = function () {
         this.balotasSacadas = new Array(75).fill(false);
-
+        this.pendientes = 75;
     }
-    // metodo  para las balotas
+
+    //metodo para sacar balotas
     this.sacarBalota = function () {
         let totalSacadas = this.balotasSacadas.filter(valor => valor == true).length;
         if (totalSacadas < 75) {
             let balota = Math.floor(Math.random() * 75) + 1;
-            
             while (this.balotasSacadas[balota - 1]) {
-                
                 balota = Math.floor(Math.random() * 75) + 1;
             }
-
             this.balotasSacadas[balota - 1] = true;
             this.ultimaBalota = balota;
+            this.pendientes = 75 - totalSacadas - 1;
         }
         else {
             window.alert("Ya se acabaron las balotas");
         }
-
     }
 }
